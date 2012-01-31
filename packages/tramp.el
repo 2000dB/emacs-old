09fb0478db33466b4c4add42d5aa00887a1b0324
Vincent de Belleval <2000db@gmail.com>
2012-01-31 10:21:31 -0500

    small mods to init

---
+++ packages/tramp.el
@@ -0,0 +1,27 @@
+;; -*- emacs-lisp -*-
+;; Tramp connection history.  Don't change this file.
+;; You can delete it, forcing Tramp to reapply the checks.
+
+((["scpc" nil "sudo" nil]
+  nil)
+ (["su" "root" "localhost" nil]
+  nil)
+ (["sudo" "root" "eski-coa1.gateway.2wire.net" nil]
+  ("uname" "Darwin 10.8.0")
+  ("test" "test")
+  ("remote-path"
+   ("/usr/bin" "/bin" "/usr/sbin" "/sbin" "/usr/local/bin"))
+  ("remote-shell" "/bin/sh")
+  ("stat" nil)
+  ("perl-file-spec" t)
+  ("perl-cwd-realpath" t)
+  ("perl" "\\perl")
+  ("id" "/usr/bin/id")
+  ("gid-integer" 0)
+  ("readlink" nil)
+  ("file-exists" "test -e")
+  ("uid-integer" 0)
+  ("local-encoding" base64-encode-region)
+  ("local-decoding" base64-decode-region)
+  ("remote-encoding" tramp-perl-encode-with-module)
+  ("remote-decoding" tramp-perl-decode-with-module)))
